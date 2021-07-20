2a2609fddae7231d549306884b742359b830b975
Todd Stellanova <tstellanova@users.noreply.github.com>
2021-07-20 13:23:29 -0700

    better ext LED status, remove uv sensor

--- src/main.cpp
+++ src/main.cpp
@@ -25,10 +25,10 @@
 
 #include "Adafruit_BME280.h"
 
-// 
+// Gas sensor
 #include "Air_Quality_Sensor.h"
 // UV sensor
-#include "Adafruit_VEML6070.h"
+// #include "Adafruit_VEML6070.h"
 
 SYSTEM_THREAD(ENABLED);
 SYSTEM_MODE(SEMI_AUTOMATIC); 
@@ -62,7 +62,7 @@ ChainableLED led_chain(EXTLED_CLOCK_PIN, EXTLED_DATA_PIN, NUM_EXT_LEDS);
 
 
 // ultraviolet light sensor
-Adafruit_VEML6070 uv_sensor;
+// Adafruit_VEML6070 uv_sensor;
 
 // in this app we use a Particle webhook to publish data and visualize with ubidots
 Ubidots ubidots((char*)"webhook", UBI_PARTICLE);
@@ -72,12 +72,15 @@ static float last_temp = 0;
 static float last_humidity = 0;
 static float last_airpressure = 0;
 static uint32_t last_pht_read = 0;
-static uint16_t last_uv = 0;
+// static uint16_t last_uv = 0;
 static uint32_t last_dust_recalc_ms = 0;
 static uint32_t total_window_lpo = 0;
 static uint32_t last_known_window_lpo = 0;
 static float dust_ratio = 0;
 static float dust_concentration = 0;
+
+static int32_t last_voc_qual = -1;
+static int32_t last_voc_value = 0;
 
 static SystemSleepConfiguration sleep_cfg = {};
 
@@ -111,7 +114,7 @@ void setup() {
 		Log.error("aq sensor init failed");
 	}
 
-	uv_sensor.begin(VEML6070_2_T);
+	// uv_sensor.begin(VEML6070_2_T);
 
 	led_chain.init();
 	
@@ -130,18 +133,26 @@ double readHumidity() {
 	return (double)last_humidity;
 }
 
-
-// Read ultraviolet light sensor
-static void read_uv_sensor() {
-	uint16_t cur_uv = uv_sensor.readUV();
-	if (65535 != cur_uv) { //invalid reading
-		last_uv = cur_uv;
+// Read the gas sensor
+static void read_voc_sensor() {
+	int gas_quality = voc_sensor.slope();
+	if (gas_quality >= 0) {
+		last_voc_qual = gas_quality;
+		last_voc_value = voc_sensor.getValue();
+		Log.info("voc qual: %d  val: %d", last_voc_qual, last_voc_value);
 	}
 }
 
+// Read ultraviolet light sensor
+// static void read_uv_sensor() {
+// 	uint16_t cur_uv = uv_sensor.readUV();
+// 	if (65535 != cur_uv) { //invalid reading
+// 		last_uv = cur_uv;
+// 	}
+// }
+
 // Read Pressure, Temperature, Humidity sensor
-static void read_pht_sensor()
-{
+static void read_pht_sensor() {
 	last_temp =  pht_sensor.readTemperature();
 	last_humidity = pht_sensor.readHumidity();
 	last_airpressure = pht_sensor.readPressure() / 100.0F;
@@ -172,8 +183,6 @@ static void read_dust_sensor() {
 		total_window_lpo = 0;
 		last_dust_recalc_ms = millis();
 	}
-
-
 }
 
 
@@ -218,12 +227,15 @@ static void sleep_control(uint32_t sleep_ms) {
   }
 }
 
+// Send sensor data via Particle.publish to ubidots
 static bool publish_data() {
 	
 	ubidots.add((char*)"temp", last_temp); 
 	ubidots.add((char*)"press", last_airpressure); 
     ubidots.add((char*)"humidity", last_humidity);
-	ubidots.add((char*)"uv", last_uv);
+	// ubidots.add((char*)"uv", last_uv);
+
+	ubidots.add((char*)"gas",last_voc_value);
 
 	if (last_known_window_lpo > 0) {
       ubidots.add((char*)"dust-lpo", last_known_window_lpo);
@@ -239,27 +251,51 @@ static bool publish_data() {
 /* This function loops forever --------------------------------------------*/
 void loop()
 {
-	led_chain.setColorRGB(0, 0,8,0);
+	// temporarily change color to indicate we're recalculating 
+	led_chain.setColorRGB(0, 0, 2, 0);
 	// connect if we aren't already connected
 	if (!Particle.connected()) {
 		Log.warn("reconnect");
 		Particle.connect(); //start connection
 	}
 
-	read_uv_sensor();
+	// read_uv_sensor();
     read_pht_sensor();
 	read_dust_sensor();
+	read_voc_sensor();
 
 	// display a "health check" status for air quality
-	if (last_pht_read > 0) {
-		uint8_t red_val = (uint8_t)(last_temp*2.55f); //( temp  / 100)  * 255;
-		uint8_t green_Val = 16;
-		uint8_t blue_val = (uint8_t)(last_humidity*2.55f);  // (humidity/100) * 255;
-		led_chain.setColorRGB(0, red_val, green_Val, blue_val);
+	uint8_t red_val = 0, green_val = 0, blue_val = 0;;
+
+	if (last_voc_qual >= 0) {
+		switch (last_voc_qual) {
+			case 0:
+				red_val = 255;
+				break;
+			case 1:
+				red_val = 128;
+				break;
+			case 2:
+				red_val = 64;
+				break;
+		} 
 	}
-	else {
-		led_chain.setColorRGB(0, 32,0,0);
+
+	if (last_dust_recalc_ms > 0) {
+		float dust_frac = dust_concentration / 500.0;
+		if (dust_frac > 1.0) dust_frac = 1.0;
+		Log.info("dustfrac: %f", dust_frac);
+		blue_val = (uint8_t)ceilf(255.0 * dust_frac);
 	}
+
+	// pin color to all green if "bad" values are acceptable
+	if ((red_val == 0) && (blue_val < 64)) {
+		green_val = 128;
+		blue_val = 0;
+	}
+		
+	led_chain.setColorRGB(0, red_val, green_val, blue_val);
+
 
 	for (int i = 0; i < 30; i++) {
 		if (Particle.connected()) { break; }
@@ -272,8 +308,8 @@ void loop()
 	}
 
 	if (publish_data()) {
-		// sleep_control(10000);
-		delay(5000);
+		sleep_control(10000);
+		// delay(5000);
 	}
 	else {
 		Log.warn("pub failed");
